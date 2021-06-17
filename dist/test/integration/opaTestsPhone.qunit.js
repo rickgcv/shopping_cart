f4025deaa0c7f84dc879b5cebff6a3158e66182a
rickgcv <rgarciac@redhat.com>
2021-06-17 16:16:26 +0100

    Add Dockerfile

---
+++ dist/test/integration/opaTestsPhone.qunit.js
@@ -0,0 +1,1 @@
+QUnit.config.autostart=false;sap.ui.getCore().attachInit(function(){"use strict";sap.ui.require(["socreation_test/SO_Create/test/integration/PhoneJourneys"],function(){QUnit.start()})});
